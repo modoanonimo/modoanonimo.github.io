87e569a4f242c416c7c76c9fbdbefa2f0d9d75e0
modoanonimo <rafaelapmfranca@gmail.com>
2023-06-18 19:22:09 -0300

    fdfd

--- login.js
+++ login.js
@@ -1,34 +1,17 @@
 
 let users = [];
 
+function redirectTime() {
 
-fetch("https://api.airtable.com/v0/appZq2F4YYkLgYbQJ/Accounts", {
-
-    headers: {
-        Authorization: 'Bearer keyDZqZcB5vKRI8xb',
-    }
-
-}).then((resp) => {
-    return resp.json()
-}).then((data) => {
-    alunos = data.records
-    alunos.map((aluno) => {
-        return users.push(aluno.fields);
-    })
-   
+    window.location.href = "./acesso-ao-monitoramento.html"
 }
 
-)
+function checkUser(aluno){
 
-
-
-function Logar(event) {
-    
-    event.preventDefault();
-    
     let user = document.querySelector(".user").value
     let password = document.querySelector(".password").value
     let passwordErr = document.querySelector(".passwordempty").value
+
 
     if (user == "") {
         userEmpty = document.querySelector(".userempty").innerHTML = "Insira Seu Usuario"
@@ -38,32 +21,68 @@ function Logar(event) {
         passwordErr = document.querySelector(".passwordempty").innerHTML = "Insira Seu Usuario"
     }
 
-    const userData = users.find(item => item.Email == user);
-
-
-    if (!userData) {
-        // return alert("Usuário e/ou senha inválidos!");
+    if (password !== aluno.fields.Senha || user !== aluno.fields.Email) {
         document.querySelector(".errorUser").style.display = "block";
+
+
     }
-
-    if (password == userData.Senha & user == userData.Email) {
-        localStorage.setItem("Cpf", userData.Cpf)
-        localStorage.setItem("Senha", password)
-        localStorage.setItem("Nome",userData.NomeAlvo)
-        localStorage.setItem("Alvo",userData.Alvo)
-        localStorage.setItem("Liberado","usuario ok")
-        redirectTime();
-
-    } else {
-        // return alert("Usuário e/ou senha inválidos!");
-        document.querySelector(".errorUser").style.display = "block";
-    }
-
 }
 
-function redirectTime() {
 
-    window.location.href = "./acesso-ao-monitoramento.html"
+
+
+
+function Logar(event) {
+
+    
+
+    event.preventDefault();
+
+    let user = document.querySelector(".user").value
+    let password = document.querySelector(".password").value
+    let passwordErr = document.querySelector(".passwordempty").value
+
+
+    // if (!userData) {
+    //     // return alert("Usuário e/ou senha inválidos!");
+    //     document.querySelector(".errorUser").style.display = "block";
+    // }
+
+
+    
+
+    fetch("https://api.airtable.com/v0/appZq2F4YYkLgYbQJ/Accounts", {
+
+        headers: {
+            Authorization: 'Bearer keyDZqZcB5vKRI8xb',
+        }
+
+    }).then((resp) => {
+        return resp.json()
+    }).then((data) => {
+
+        alunos = data.records
+        alunos.map( (aluno) => {
+            // console.log(aluno.fields.Senha)
+            console.log(aluno.fields.Email)
+
+            checkUser(aluno);
+
+            if (password === aluno.fields.Senha & user === aluno.fields.Email) {
+
+                localStorage.setItem("Cpf", aluno.fields.Cpf)
+                localStorage.setItem("Senha", password)
+                localStorage.setItem("Nome", aluno.fields.NomeAlvo)
+                localStorage.setItem("Alvo", aluno.fields.Alvo)
+                localStorage.setItem("Status", aluno.fields.Status)
+                localStorage.setItem("Liberado", "usuario ok")
+                redirectTime();
+
+            }
+        }
+        )
+    }
+    )
 }
 
 
@@ -73,8 +92,8 @@ function closeAlert() {
 
 
 
-function Click (){
-    
+function Click() {
+
     const passwordInput = document.querySelector("#password")
     const eye = document.querySelector("#eye")
 
@@ -82,4 +101,4 @@ function Click (){
     eye.classList.toggle("fa-eye-slash")
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
     passwordInput.setAttribute("type", type)
-  }
+}
